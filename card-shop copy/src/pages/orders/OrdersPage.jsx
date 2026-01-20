986117b4a0f73067cf1053e69f447c3d9baed437
Angelika <alekseevaa11t@gmail.com>
2026-01-20 16:32:18 +0300

    Convert project to TypeScript

---
+++ card-shop copy/src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,136 @@
+import axios from "axios";
+import dayjs from "dayjs";
+import { useState, useEffect, Fragment } from "react";
+import Header from "../../components/Header";
+import { Link } from "react-router";
+import "./OrdersPage.css";
+import { formatMoney } from "../../utils/money";
+
+const OrdersPage = ({ cart }) => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/orders?expand=products").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <title>Orders</title>
+
+      <Header cart={cart} />
+
+      <div className="header">
+        <div className="left-section">
+          <Link to="/" className="header-link">
+            <img className="logo" src="images/logo-white.png" />
+            <img className="mobile-logo" src="images/mobile-logo-white.png" />
+          </Link>
+        </div>
+
+        <div className="middle-section">
+          <input className="search-bar" type="text" placeholder="Search" />
+
+          <button className="search-button">
+            <img className="search-icon" src="images/icons/search-icon.png" />
+          </button>
+        </div>
+
+        <div className="right-section">
+          <Link className="orders-link header-link" to="/orders">
+            <span className="orders-text">Orders</span>
+          </Link>
+
+          <Link className="cart-link header-link" to="/checkout">
+            <img className="cart-icon" src="images/icons/cart-icon.png" />
+            <div className="cart-quantity">3</div>
+            <div className="cart-text">Cart</div>
+          </Link>
+        </div>
+      </div>
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+
+        <div className="orders-grid">
+          {orders.map((order) => {
+            return (
+              <div key={order.id} className="order-container">
+                <div className="order-header">
+                  <div className="order-header-left-section">
+                    <div className="order-date">
+                      <div className="order-header-label">Order Placed:</div>
+                      <div>{dayjs(order.orderTimeMS).format("MMMM D")}</div>
+                    </div>
+                    <div className="order-total">
+                      <div className="order-header-label">Total:</div>
+                      <div>{formatMoney(order.totalCostCents)}</div>
+                    </div>
+                  </div>
+
+                  <div className="order-header-right-section">
+                    <div className="order-header-label">Order ID:</div>
+                    <div>{order.id}</div>
+                  </div>
+                </div>
+
+                <div className="order-details-grid">
+                  {orders.map((order) =>
+                    order.products.map((orderProduct) => (
+                      <Fragment key={orderProduct.productId}>
+                        <div className="product-image-container">
+                          <img
+                            src={`http://localhost:3000/${orderProduct.product.image}`}
+                            alt={orderProduct.product.name}
+                          />
+                        </div>
+
+                        <div className="product-details">
+                          <div className="product-name">
+                            {orderProduct.product.name}
+                          </div>
+
+                          <div className="product-delivery-date">
+                            Arriving on:{" "}
+                            {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
+                              "MMMM D",
+                            )}
+                          </div>
+
+                          <div className="product-quantity">
+                            Quantity: {orderProduct.quantity}
+                          </div>
+
+                          <button className="buy-again-button button-primary">
+                            <img
+                              className="buy-again-icon"
+                              src="/images/icons/buy-again.png"
+                              alt=""
+                            />
+                            <span className="buy-again-message">
+                              Add to Cart
+                            </span>
+                          </button>
+                        </div>
+
+                        <div className="product-actions">
+                          <a href="/tracking">
+                            <button className="track-package-button button-secondary">
+                              Track package
+                            </button>
+                          </a>
+                        </div>
+                      </Fragment>
+                    )),
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OrdersPage;
